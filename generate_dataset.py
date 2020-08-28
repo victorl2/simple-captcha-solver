f8ef559dcc3d36b8029a5237d730bc4d4ff4a29b
victor silva <victorfts@id.uff.br>
2020-08-28 10:20:38 -0300

    including auto install of chrome driver

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from PIL import Image
 from io import BytesIO
@@ -15,6 +16,8 @@ CHROME_OPTIONS.add_argument('lang')
 CHROME_OPTIONS.add_argument("--disable-gpu")
 CHROME_OPTIONS.add_argument("start-maximized")
 CHROME_OPTIONS.add_argument('--disable-dev-shm-usage')
+CHROME_DRIVER = ChromeDriverManager().install()
+
 
 
 def extract_catpcha_image(captcha_page_url: str) -> Image:
@@ -25,7 +28,7 @@ def extract_catpcha_image(captcha_page_url: str) -> Image:
     :param captcha_page_url: url to the page that contains the captcha 
     :return: A PIL image
     """""
-    browser = webdriver.Chrome(options=CHROME_OPTIONS)
+    browser = webdriver.Chrome(CHROME_DRIVER, options=CHROME_OPTIONS)
     browser.get(captcha_page_url)
     base64_img_data = browser.find_element_by_xpath("//td[@align='right']/img[@height='24']").get_attribute('src')
     parsed_base64_data = base64_img_data.replace('data:image/png;base64,', '')
@@ -46,13 +49,13 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         raise Exception('It is required to pass the link to extract the captcha as a command line argument. '
                         'Try running: python3 generate_dataset.py [PAGE_URL] [TOTAL_CAPTCHAS_TO_COLLECT]')
-    captcha_page_url = sys.argv[1]
+    captcha_url_page = sys.argv[1]
     total_captchas = int(sys.argv[2])
     print('#### Captcha dataset generator ####')
-    print('scrapping captchas from {}'.format(captcha_page_url))
+    print('scrapping captchas from {}'.format(captcha_url_page))
 
     for index in range(total_captchas):
         start_time = timeit.default_timer()
-        captcha_image = extract_catpcha_image(captcha_page_url)
+        captcha_image = extract_catpcha_image(captcha_url_page)
         save_image(captcha_image)
         print('captcha {} extracted in {:.2f} seconds'.format(index, timeit.default_timer() - start_time))
